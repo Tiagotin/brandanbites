26f631e9db219acd9d986fe3b73a4a53e180ff8a
MoraEiSan <eitansantino06@gmail.com>
2024-10-21 00:36:44 -0300

    usuarios

--- src/models/seed.js
+++ src/models/seed.js
@@ -12,63 +12,63 @@ async function insertUsers() {
       {
         nombre: 'Santino Moraes',
         email: '47216695@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 3379-3285',
         rol: 'admin', // Asignar rol de administrador
       },
       {
         nombre: 'Tiago Guagliano',
         email: '46557799@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 6129-2269',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Lucía Miranda',
         email: '47307777@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 3656-2248',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Damian Perez',
         email: '47058200@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 5119-2369',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Santiago Benitez',
         email: '46991998@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 5814-4020',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Mauro Farias',
         email: '47156059@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 5919-1823',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Santiago Sosa',
         email: '47236674@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 2835-3181',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Gastón Paredes',
         email: '47027962@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 3775-0261',
         rol: 'admin', // Asignar rol de usuario normal
       },
       {
         nombre: 'Marsi Ferreira',
         email: '47414885@insi.edu.ar',
-        password: 'LoKu2dns0F!',
+        password: 'De7330de',
         telefono: '11 2609-8649',
         rol: 'admin', // Asignar rol de usuario normal
       },
